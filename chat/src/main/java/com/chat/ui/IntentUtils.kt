3d6e7fcde57e2740f34787ecdbbcb2f3638861a5
alexei.artsimovich <alexei.artsimovich@gmail.com>
2023-04-09 11:27:41 +0200

    fixed copy feature for single message

--- chat/src/main/java/com/chat/ui/IntentUtils.kt
+++ chat/src/main/java/com/chat/ui/IntentUtils.kt
@@ -10,21 +10,28 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 
-private suspend fun createText(context: Context, messages: List<Message>): String {
-    val userCaption = context.getString(R.string.user)
-    val botCaption = context.getString(R.string.bot)
-    return withContext(Dispatchers.Default) {
-        messages.joinToString(separator = "\n\n") { message ->
-            (if (message.isFromUser) userCaption else botCaption) +
-                    '\n' + MessageDateUtils.getDateText(message) +
-                    '\n' + message.text
+private suspend fun createText(context: Context, messages: List<Message>, addMetaInfo: Boolean): String {
+    val separator = "\n\n"
+    return if (addMetaInfo) {
+        val userCaption = context.getString(R.string.user)
+        val botCaption = context.getString(R.string.bot)
+        withContext(Dispatchers.Default) {
+            messages.joinToString(separator = separator) { message ->
+                (if (message.isFromUser) userCaption else botCaption) +
+                        '\n' + MessageDateUtils.getDateText(message) +
+                        '\n' + message.text
+            }
+        }
+    } else {
+        withContext(Dispatchers.Default) {
+            messages.joinToString(separator = separator) { it.text }
         }
     }
 }
 
 @SuppressLint("QueryPermissionsNeeded")
 suspend fun Context.shareMessages(messages: List<Message>): Boolean {
-    val text = createText(this, messages)
+    val text = createText(this, messages, addMetaInfo = true)
     return withContext(Dispatchers.Main) {
         val intent = Intent(Intent.ACTION_SEND)
         intent.type = "text/plain"
@@ -47,7 +54,7 @@ suspend fun Context.copyMessagesToClipboard(messages: List<Message>): Boolean {
     val clipboard: ClipboardManager =
         ContextCompat.getSystemService(this, ClipboardManager::class.java) ?: return false
     val label = getString(R.string.messages)
-    val text = createText(this, messages)
+    val text = createText(this, messages, addMetaInfo = messages.count() > 1)
     val clip = ClipData.newPlainText(label, text)
     clipboard.setPrimaryClip(clip)
     return true
